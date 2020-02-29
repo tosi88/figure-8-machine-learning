a4e6315b97005f98a6aeb3a79b9479dca34bcdcb
Tobias Siekmann <tobias.siekmann.ts@gmail.com>
2020-02-29 12:44:52 +0100

    add classifier

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# figure-8-machine-learning
+# Disaster Response Pipeline Project
 This should provide a solution for the udacity data science nano-degree project. The project consists of messaging data for desasters from Figure-eight. 
 
+## Instructions:
+1. Run the following commands in the project's root directory to set up your database and model.
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
 
-## 1. Installations
-You need to have the following dependencies installed with pip3:
-- jupyter notebook
-- numpy
-- pandas
-- sklearn
-
-
+2. Run the following command in the app's directory to run your web app.
+    `python run.py`
+3. Go to http://0.0.0.0:3001/
 
 ## 2. Project Motivation
 The underlying data set is a set of emergency text messages. In case of major catastrophic events, incidents are reported by thousands of people. In order to distinguish already known incidents and information from newly incoming relevant news, we use a machine learning pipeline. 
 First we clean the data set in an ETL pipeline and afterwards we use the pipeline to train a model that is able to decide weather newly incoming messages provide relevant information.
 
-## 3. File Descriptions
-- categories.csv the categories that a message belongs to.
-- messages.csv - the disaster messages. 
-
-
-## 4. How to Interact with your project
-
 
 ## 5. Licensing, Authors, Acknowledgements, etc.
 MIT Licensing - special thanks to Udacity and Figure-Eight for providing the data set.
+
+
+
+
+
+
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,98 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
 
+
+import re
+import pickle
+
+import nltk
+import nltk
+#nltk.download()
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import classification_report
 
 def load_data(database_filepath):
-    pass
+    """ load data from sqlite database and split into features (messages) and targets (categories)
+    Input:  filepath of the database
+    Output: features(X), targets(Y) and category names
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table("DisasterResponse", engine)
+    category_names = df.columns[-36:]
+    X = df["message"]
+    y = df[category_names]
+    return X,y, category_names
 
 
 def tokenize(text):
-    pass
-
+    """process text, tokenize, remove stopwords and reduces words to their root form with lemmatizing
+    Input: text as string
+    Output: cleaned words as array
+    """
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    #Tokenize
+    words = nltk.word_tokenize(text)
+    #Remove stopwords
+    words= [w for w in words if w not in stopwords.words("english")]
+    # Reduce words to their root form
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(lemmatizer.lemmatize(word)) for word in words]
+    return words
 
 def build_model():
-    pass
+    """
+    Build model for classification of messages with help of a sklear pipeline, GridSearch and optimized paramters
+    output: model
+    """
+    pipeline = Pipeline([
+                    ('vect', CountVectorizer(tokenizer=tokenize)),
+                    ('tfidf', TfidfTransformer()),
+                    ('clf', MultiOutputClassifier(estimator=AdaBoostClassifier()))
+    ])
 
+    parameters = {
+                'vect__min_df':[1,10,50],
+                'clf__estimator__learning_rate': [0.001, 0.01, 0.1],
+                'tfidf__smooth_idf': [True, False]
+    }
+    return GridSearchCV(pipeline, param_grid=parameters, cv=2)
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    """evaluate model against test-set with classification report
+    Input:
+        - model: trained model
+        - X_test: test features (
+        - y_test: test targets
+        - category_names (categories
+    Output: classification report with precision, recall, f1_score and support metrics
+    """
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names, digits=2))
 
 
 def save_model(model, model_filepath):
-    pass
+    """save model to pickle file
+    input:
+        - model: trained model
+        - model_filepath: filepath to save a pickle file
+    """
+    with open(model_filepath, 'wb') as pickle_file:
+                  pickle.dump(model, pickle_file)
+    pickle_file.close()
 
 
 def main():
@@ -27,13 +101,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
